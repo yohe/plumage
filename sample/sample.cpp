c024f2726e153b363b9f63266a4ed9ad7e64d4c9
yohe <ysekiguchi.zzz@gmail.com>
2013-06-26 06:44:30 +0900

    Update.

--- sample/sample.cpp
+++ sample/sample.cpp
@@ -6,7 +6,11 @@
 int main(int argc, char const* argv[])
 {
     plumage::PluginManager manager;
+#ifdef MAC_OSX
     manager.loadPlugin("./libsample_plugin.dylib", "createSamplePlugin");
+#else
+    manager.loadPlugin("./libsample_plugin.so", "createSamplePlugin");
+#endif
 
     plumage::PluginRepository* repos = manager.getPluginRepository("SamplePlugin", 1, true);
     SamplePlugin* pif = repos->getPlugin<SamplePlugin>();
